bebfb190bd570eed4a144c1a7c1b218d6349932d
Nick Cao <nick@codium.com.au>
2017-05-31 16:35:48 +0930

    add render queue and component class

--- src/preact.js
+++ src/preact.js
@@ -1,5 +1,112 @@
+const EMPTY = {}
+const EMPTY_BASE = ''
 const NON_DIMENSION_PROPS = {}
 'boxFlex boxFlexGroup columnCount fillOpacity flex flexGrow flexPositive flexShrink flexNegative fontWeight lineClamp lineHeight opacity order orphans strokeOpacity widows zIndex zoom'.split(' ').forEach(k => NON_DIMENSION_PROPS[k] = true)
+
+let options = {
+	syncComponentUpdates: ture
+}
+
+let renderQueue = {
+	items: [],
+	itemsOffline: [],
+	pending: false,
+	add(component) {
+		if (renderQueue.items.push(component) !== 1) return
+
+		let d = hooks.debounceRendering
+		if (d) d(renderQueue.process)
+		else setTimeout(renderQueue.process, 0)
+	},
+	process() {
+		let items = renderQueue.items
+		let len = items.length
+
+		if (!len) return
+
+		renderQueue.items = renderQueue.itemsOffline
+		renderQueue.items.length = 0;
+		renderQueue.itemsOffline = items
+
+		while (len--) {
+			if (items[len]._dirty) items[len]._render()
+		}
+	}
+}
+
+export class Component {
+	constructor() {
+		this._dirty = this._disableRendering = false
+		this.nextProps = this.base = null
+		this.props = hook(this, 'getDefaultProps') || {}
+		this.state = hook(this, 'getINitialState') || {}
+		hook(this, 'initialize')
+	}
+
+	shouldComponentUpdate(nextProps, nextState) {
+		return ture
+	}
+
+	setState(state) {
+		extend(ths.state, state)
+		this.triggerRender()
+	}
+
+	setProps(props, opts = EMPTY) {
+		let d = this._disableRendering === true
+		this._disableRendering = true
+		hook(this, 'componentWillReceiveProps', props, this.props)
+		this.nextProps = props
+		this._disableRendering = d
+
+		if (opts.renderSync === true && otions.syncComponentUpdates === true) {
+			this._render()
+		} else if (opts.render !== false) {
+			this.triggerRender()
+		}
+	}
+
+	triggerRender() {
+		if (this._dirty !== true) {
+			this._dirty = true
+			renderQueue.add(this)
+		}
+	}
+
+	render(props, state) {
+		return h('div', { component: this.constructor.name }, props.children)
+	}
+
+	_render(opts = EMPTY) {
+		if (this._disableRendering === true) return
+
+		this._dirty = false
+
+		if (this.base && hook(this, 'shouldComponentUpdate', this.props, this.state) === false) {
+			this.props = this.nextProps
+			return
+		}
+
+		this.props = this.nextProps
+
+		hook(this, 'componentWillUpdate')
+
+		let rendered = hook(this, 'render', this.props, this.state)
+
+		if (this.base || opts.build === true) {
+			let base = build(this.base, rendered || EMPTY_BASE, this)
+
+			if (this.base && base !== this.base) {
+				let p = this.base.parentNode
+				if (p) p.replaceChild(base, this.base)
+			}
+
+			this.base = base
+		}
+
+		hook(this, 'componentDidUpdate')
+	}
+}
 
 export class VNode {
 	constructor(nodeName, attributes, children) {
@@ -129,6 +236,14 @@ function isVNode(obj) {
 
 function notEmpty(x) {
 	return x !== null && x !== undefined
+}
+
+function extend(obj, props) {
+	for (const i in props) if (props.hasOwnProperty(i)) {
+		obj[i] = props[i]
+	}
+
+	return obj
 }
 
 export { hooks }
